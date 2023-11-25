3ec4b2ae83ec9004358c31c8741f4481e1502545
mpratama17 <yogmhmmd17@gmail.com>
2023-11-25 14:25:12 +0800

    create list users ui

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,17 +6,52 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final HomeController state = Get.put(HomeController());
+    final ScrollController _scrollController = ScrollController();
+    state.getUser();
+
+    void onScroll() {
+      double maxScroll = _scrollController.position.maxScrollExtent;
+      double currentScroll = _scrollController.position.pixels;
+
+      if (maxScroll == currentScroll && state.hasMore.value) {
+        state.getUser();
+      }
+    }
+
+    _scrollController.addListener(onScroll);
+
     return Scaffold(
         appBar: AppBar(
-          title: const Text('HomeView'),
-          centerTitle: true,
+          title: Text('List User'),
+          // centerTitle: true,
         ),
-        body: Column(
-          children: [
-            Center(child: Text("halo")),
-          ],
+        body: Obx(
+          () => ListView.builder(
+              controller: _scrollController,
+              itemCount: state.hasMore.value
+                  ? state.users.length + 1
+                  : state.users.length,
+              itemBuilder: (context, index) {
+                if (index < state.users.length) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(state.users[index].avatar ?? ''),
+                    ),
+                    title: Text(state.users[index].name.toString()),
+                    subtitle: Text(state.users[index].email.toString()),
+                  );
+                } else {
+                  // return circularProgressIndicator();
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }),
         ));
   }
 }
