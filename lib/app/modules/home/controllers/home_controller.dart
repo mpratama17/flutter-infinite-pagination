f6486e7d7c839a51c179fe635d517d378e01bc3a
mpratama17 <yogmhmmd17@gmail.com>
2023-11-25 14:23:25 +0800

    setup controller

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,10 +1,37 @@
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
+import 'package:wehealth/app/data/user.dart';
 import 'package:wehealth/app/data/user_repository.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
   final UserRepository _userRepository = UserRepository();
+  final int _limit = 15;
+  int _page = 1;
+  var users = <User>[].obs;
+  var hasMore = true.obs;
+
+  Future getUser() async {
+    try {
+      List<User> response = await _userRepository.fetchUsers(_page, _limit);
+      if (response.length < _limit) {
+        hasMore.value = false;
+      }
+
+      users.addAll(response);
+      _page++;
+    } catch (e) {
+      if (kDebugMode) print(e.toString());
+    }
+
+    Future refreshData() async {
+      _page = 1;
+      hasMore.value = true;
+      users.value = [];
+      await getUser();
+    }
+  }
 
   final count = 0.obs;
   @override
